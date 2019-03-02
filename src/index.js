3e368e4457c052e247e457860e22d496c422e95e
toshiba <249952@qq.com>
2019-03-02 22:42:06 +0800

    点击的时候Ｘ不对齐，不知道是不是ＣＳＳ有问题

--- src/index.js
+++ src/index.js
@@ -2,19 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 class Square extends React.Component {
+    
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
-  }
-  
+}
+
 class Board extends React.Component {
-    renderSquare(i) {
-      return <Square />;
+    constructor() {
+      super()
+      this.state = {
+        squares: Array(9).fill(null),
+      };
     }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
+    renderSquare(i) {
+      return (
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />)
+    }
+
   
     render() {
       const status = 'Next player: X';
@@ -40,7 +59,7 @@ class Board extends React.Component {
         </div>
       );
     }
-  }
+}
   
 class Game extends React.Component {
     render() {
@@ -56,12 +75,12 @@ class Game extends React.Component {
         </div>
       );
     }
-  }
+}
   
   // ========================================
   
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
   
